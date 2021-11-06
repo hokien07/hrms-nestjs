49d3f9a0f074bb5ec0043ca57b1fe73a38c73478
kienhs <kienhs@dolphinsolutions.vn>
2021-11-06 15:55:14 +0700

    Success struct of views

--- src/main.ts
+++ src/main.ts
@@ -2,38 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-import compression from 'fastify-compress';
-import { sessionConfig } from '../config/session.config';
-import secureSession from 'fastify-secure-session';
 import { join } from 'path';
 
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function main() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({ logger: true }),
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.use(helmet());
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
-  await app.register(secureSession, sessionConfig);
-  await app.register(compression, { encodings: ['gzip', 'deflate'] });
-  app.useStaticAssets({
-    root: join(__dirname, '../..', 'public'),
-    prefix: '/public/',
-  });
-  app.setViewEngine({
-    engine: {
-      handlebars: require('handlebars'),
-    },
-    templates: join(__dirname, '../..', 'views'),
-  });
+  app.useStaticAssets(join(__dirname, '../..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '../..', 'views'));
+  app.setViewEngine('hbs');
+  app.set('view options', { layout: 'layouts/main' });
   await app.listen(process.env.PORT || 3000);
 }
 main();
